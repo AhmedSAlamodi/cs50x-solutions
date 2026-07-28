43a7bff9b1b7a963c6604885dc7151a76b0fc198
AHMED SALAH AL-AMODI <ahmedsalahalamodi@gmail.com>
2026-07-28 13:55:54 +0300

    Add Mario pyramid height input and printing logic

---
+++ mario.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1 || n > 8);
+    for (int row = 0; row < n; row++)
+    {
+        for (int space = 0; space < n - row - 1; space++)
+        {
+            printf(" ");
+        }
+        for (int column = 0; column <= row; column++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
